dafdb0f510fddca1c89d1f3e8198d8a9d2cae53e
Topy <Clement.Orlandini.Etu@univ-lemans.fr>
2020-03-25 01:43:09 +0100

    Fixed links in navbar
    added /wiki/ to make them absolute but from wiki rather than root

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -2,13 +2,12 @@
 document.addEventListener("DOMContentLoaded", e => {
   const radios = document.querySelectorAll(".accordion [type='radio']");
 
-  for (let radio in radios) {
-    debug.log(radio);
-    radio.addEventListener("click", function(e) {
-      debug.log("clicked");
-      if (this.checked) {
-        this.checked = false;
-      }
+  for (let radio of radios) {
+    radio.addEventListener("mousedown", e => {
+      e.preventDefault();
+    });
+    radio.addEventListener("click", e => {
+      //this.checked = !this.checked;
     });
   }
 });
